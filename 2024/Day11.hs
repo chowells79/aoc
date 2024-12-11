204490aca7cc5481c505df41bcaaf0be8efb35cb
Carl Howells <chowells79@gmail.com>
2024-12-10 23:03:16 -0800

    Day 11

---
+++ 2024/Day11.hs
@@ -0,0 +1,48 @@
+#!/usr/bin/env cabal
+{- cabal:
+build-depends: base, containers
+-}
+
+import Data.List (iterate')
+import Data.Map (Map, fromListWith, toList)
+
+input :: Integer -> IO String
+input n = readFile name
+  where
+    name | n == 0 = "input/" ++ ident ++ ".txt"
+         | otherwise = "example/" ++ ident ++ "-" ++ show n ++ ".txt"
+    ident = "11"
+
+parse :: String -> Map Integer Integer
+parse = fromListWith (+) . flip zip (repeat 1) . map read . words
+
+step :: Integer -> [Integer]
+step i
+    | i == 0 = [1]
+    | r == 0 = map read [ui, li]
+    | otherwise = [i * 2024]
+  where
+    is = show i
+    lis = length is
+    (q, r) = lis `quotRem` 2
+    (ui, li) = splitAt q is
+
+blink :: Map Integer Integer -> Map Integer Integer
+blink prev = fromListWith (+)
+    [ (n, count)
+    | (i, count) <- toList prev
+    , n <- step i
+    ]
+
+multiblink :: Int -> Map Integer Integer -> Map Integer Integer
+multiblink n = head . drop n . iterate' blink
+
+solve :: Int -> Map Integer Integer -> Integer
+solve n = sum . multiblink n
+
+main :: IO ()
+main = do
+    inp <- parse <$> input 0
+
+    print $ solve 25 inp
+    print $ solve 75 inp
