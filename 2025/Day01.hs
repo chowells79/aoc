70c29b0713cc7102f5b9e52aacc9c113788e7a6a
Carl Howells <chowells79@gmail.com>
2025-11-30 19:04:32 -0800

    Set up for 2025

---
+++ 2025/Day01.hs
@@ -0,0 +1,28 @@
+#!/usr/bin/env cabal
+{- cabal:
+build-depends: base
+-}
+
+import Text.ParserCombinators.ReadP
+import Data.Char (isDigit)
+
+input :: Int -> IO String
+input n = readFile name
+  where
+    name | n == 0 = "input/01.txt"
+         | otherwise = "example/01-" ++ show n ++ ".txt"
+
+parse :: String -> String
+parse s = case readP_to_S full s of
+            [(x, "")] -> x
+            x -> error $ "Parse error: " ++ (show x)
+  where
+    full = munch (const True) <* eof
+
+    num :: (Read a, Num a) => ReadP a
+    num = read <$> munch1 isDigit
+
+main :: IO ()
+main = do
+    inp <- parse <$> input 1
+    print inp
