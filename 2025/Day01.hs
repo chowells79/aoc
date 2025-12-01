ffd7ae2dc72fb7c15dfdec6662be323561bed02a
Carl Howells <chowells79@gmail.com>
2025-11-30 22:42:56 -0800

    Day 1 2025

--- 2025/Day01.hs
+++ 2025/Day01.hs
@@ -5,6 +5,8 @@ build-depends: base
 
 import Text.ParserCombinators.ReadP
 import Data.Char (isDigit)
+import Control.Applicative
+import Data.List
 
 input :: Int -> IO String
 input n = readFile name
@@ -12,17 +14,36 @@ input n = readFile name
     name | n == 0 = "input/01.txt"
          | otherwise = "example/01-" ++ show n ++ ".txt"
 
-parse :: String -> String
+parse :: String -> [Int]
 parse s = case readP_to_S full s of
-            [(x, "")] -> x
+            [(x, "")] -> scanl' (+) 50 x
             x -> error $ "Parse error: " ++ (show x)
   where
-    full = munch (const True) <* eof
+    full = some line <* eof
 
-    num :: (Read a, Num a) => ReadP a
+    dir = id <$ char 'L' <|> negate <$ char 'R'
     num = read <$> munch1 isDigit
+    line = dir <*> num <* skipSpaces
+
+
+solve1 :: [Int] -> Int
+solve1 = length . filter (== 0) . map (`mod` 100)
+
+
+expand :: [Int] -> [Int]
+expand (x:xs@(y:_)) = run ++ expand xs
+  where
+    run | x < y = [x, x + 1 .. y - 1]
+        | x > y = [x, x - 1 .. y + 1]
+        | otherwise = []
+expand z = z
+
+solve2 :: [Int] -> Int
+solve2 = solve1 . expand
+
 
 main :: IO ()
 main = do
-    inp <- parse <$> input 1
-    print inp
+    inp <- parse <$> input 0
+    print $ solve1 inp
+    print $ solve2 inp
