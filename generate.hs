91ee43346eb9b72118773ca7505e3e221ce3618f
Carl Howells <chowells79@gmail.com>
2025-12-04 10:05:06 -0800

    Update generate script to create directories when needed

--- generate.hs
+++ generate.hs
@@ -22,12 +22,22 @@ import Control.Monad
 import qualified Data.Text.Template as TT
 
 import System.Posix.Files
+import System.Posix.Directory
+import System.Posix.Types
 
 main :: IO ()
 main = do
     [yearS, dayS] <- getArgs
     let year = read yearS
         day = mkDay_ (read dayS)
+
+    makeSolution year day
+    makeExample year day
+    fetchInput year day
+
+
+fetchInput :: Integer -> Day -> IO ()
+fetchInput year day = do
     sessionKeyRaw <- readFile ".session_key"
     let sessionKey = filter (not . isSpace) sessionKeyRaw
         userAgent = AoCUserAgent "https://github.com/chowells79/aoc" "chowells79@gmail.com"
@@ -38,20 +48,21 @@ main = do
         True -> do
             putStrLn "Fetching input..."
             input <- runAoC_ opts $ AoCInput day
-            let fileName = printf "%d/input/%02d.txt" year (dayInt day)
+            let dirName = printf "%d/input" year
+                fileName = printf "%s/%02d.txt" dirName (dayInt day)
+            ensureDir dirName
             T.writeFile fileName input
+            setFileMode fileName createdMode
             putStrLn "Input fetched."
         False -> putStrLn "Not released, skipping input."
-
-    makeSolution year day
-    makeExample year day
-
 
 makeSolution :: Integer -> Day -> IO ()
 makeSolution year day = do
     let ident = printf "%02d" (dayInt day)
-        outName = printf "%d/Day%s.hs" year ident
+        yearDir = printf "%d" year
+        outName = printf "%s/Day%s.hs" yearDir ident
 
+    ensureDir yearDir
     alreadyExists <- fileExist outName
     case alreadyExists of
         True -> putStrLn $
@@ -64,23 +75,38 @@ makeSolution year day = do
                 out = TT.substitute tpl ctx
 
             TL.writeFile outName out
+            setFileMode outName createdMode
 
             putStrLn "Solution file created"
-
-            setFileMode outName $ foldr unionFileModes nullFileMode [
-                ownerModes, groupReadMode, groupExecuteMode,
-                otherReadMode, otherExecuteMode ]
 
 
 makeExample :: Integer -> Day -> IO ()
 makeExample year day = do
-    let ident = printf "%02d" (dayInt day) :: String
-        outName = printf "%d/example/%s-1.txt" year ident :: String
+    let exampleDir = printf "%d/example" year
+        outName = printf "%s/%02d-1.txt" exampleDir (dayInt day)
 
+    ensureDir exampleDir
     alreadyExists <- fileExist outName
     case alreadyExists of
         True -> putStrLn $
             "Example input " ++ outName ++ " already exists. Not overwriting."
         False -> do
             T.writeFile outName $ T.pack ""
+            setFileMode outName createdMode
             putStrLn "(Empty) example input created"
+
+ensureDir :: String -> IO ()
+ensureDir dirName = do
+    alreadyExists <- fileExist dirName
+    case alreadyExists of
+        True -> pure ()
+        False -> do
+            putStrLn $ "Creating directory " ++ dirName
+            createDirectory dirName createdMode
+
+createdMode :: FileMode
+createdMode = foldl' unionFileModes nullFileMode
+    [ ownerModes
+    , groupReadMode, groupExecuteMode
+    , otherReadMode, otherExecuteMode
+    ]
