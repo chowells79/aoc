ccb17f54659baf62bc1cefa207ffdbad56746c68
Carl Howells <chowells79@gmail.com>
2026-01-07 00:42:57 -0800

    2015, Day 18

---
+++ 2015/Day18.hs
@@ -0,0 +1,61 @@
+#!/usr/bin/env cabal
+{- cabal:
+build-depends: base, containers
+-}
+
+import Text.ParserCombinators.ReadP
+import Data.Char (isDigit)
+
+import Data.List (elemIndices, iterate')
+
+import Data.Set (Set)
+import qualified Data.Set as S
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+input :: Int -> IO String
+input n = readFile name
+  where
+    name | n == 0 = "input/18.txt"
+         | otherwise = "example/18-" ++ show n ++ ".txt"
+
+parse :: String -> Set (Int, Int)
+parse s = S.fromList [ (r, c)
+                     | (r, l) <- zip [0..] (lines s)
+                     , c <- elemIndices '#' l
+                     ]
+
+neighbors :: (Int, Int) -> [(Int, Int)]
+neighbors p@(r, c) = filter (/= p) [ (r', c')
+                                   | r' <- [ r - 1, r, r + 1]
+                                   , c' <- [ c - 1, c, c + 1 ]
+                                   , r' >= 0 && r' < 100
+                                   , c' >= 0 && c' < 100
+                                   ]
+
+step :: Set (Int, Int) -> Set (Int, Int)
+step s = S.union old new
+  where
+    ns = M.fromListWith (+) [ (p', 1) | p <- S.toList s, p' <- neighbors p ]
+    (o, n) = M.partitionWithKey (\k _ -> S.member k s) ns
+    old = M.keysSet $ M.filter (\c -> c == 2 || c == 3) o
+    new = M.keysSet $ M.filter (\c -> c == 3) n
+
+solve1 :: Set (Int, Int) -> Int
+solve1 = S.size . (!! 100) . iterate step
+
+
+addCorners :: Set (Int, Int) -> Set (Int, Int)
+addCorners = S.union $ S.fromList [ (0, 0), (0, 99), (99, 99), (99, 0) ]
+
+
+solve2 :: Set (Int, Int) -> Int
+solve2 = S.size . (!! 100) . iterate (addCorners . step) . addCorners
+
+
+main :: IO ()
+main = do
+    inp <- parse <$> input 0
+    print $ solve1 inp
+    print $ solve2 inp
